219fdb2c7fb92bcd3c0d9dfadea39d492a55e476
Paul-1783 <paulschmitt144@gmail.com>
2023-03-22 15:33:17 +0100

    defined a pokemonlist and added 3 objects to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-let favoriteFood = "chicken gongabao style";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"],
+  },
+
+  {
+    name: "Zubat",
+    height: 0.8,
+    types: ["flying", "poison"],
+  },
+  {
+    name: "Charisard",
+    height: 1.7,
+    types: ["fire", "flying"],
+  },
+];
