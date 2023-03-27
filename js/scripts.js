d6941ab2406e0b6e08ef4acb55bcacde471343f1
Paul-1783 <paulschmitt144@gmail.com>
2023-03-27 22:07:51 +0200

    Introduced IIFE into scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,62 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"],
-  },
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"],
+    },
 
-  {
-    name: "Zubat",
-    height: 0.8,
-    types: ["flying", "poison"],
-  },
-  {
-    name: "Charisard",
-    height: 1.7,
-    types: ["fire", "flying"],
-  },
-];
+    {
+      name: "Zubat",
+      height: 0.8,
+      types: ["flying", "poison"],
+    },
+    {
+      name: "Charisard",
+      height: 1.7,
+      types: ["fire", "flying"],
+    },
+  ];
 
-// for (let i = 0; i < 3; ++i) {
-//   if (pokemonList[i].height > 2.5) {
-//     document.write(
-//       `${pokemonList[i].name} (${pokemonList[i].height}) - Wow, that's big!`
-//     );
-//   } else {
-//     document.write(`${pokemonList[i].name} (${pokemonList[i].height})`);
-//   }
-//   document.write("<br><br>");
-// }
+  function verifyObject(pokemon) {
+    if (typeof pokemon === "object") {
+      let allProperties = Object.keys(pokemon);
+      console.log(allProperties);
+      if (
+        allProperties.indexOf("name") !== -1 &&
+        allProperties.indexOf("height") !== -1 &&
+        allProperties.indexOf("types") !== -1
+      ) {
+        return true;
+      }
+    } else {
+      console.log("Element you tried to add to pokemonlist wasn't an object.");
+      return false;
+    }
+  }
 
-pokemonList.forEach(function (item) {
+  function add(pokemon) {
+    if (verifyObject(pokemon) === true) {
+      pokemonList.push(pokemon);
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function search(name) {
+    return pokemonList.filter((item) => item.name === name);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    search: search,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (item) {
   if (item.height > 2.5) {
     document.write(`${item.name} (${item.height}) - Wow, that's big!`);
   } else {
